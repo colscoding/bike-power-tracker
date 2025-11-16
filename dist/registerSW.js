fbab2da42f7a33370b2b24386f9ac1f0b5875606
Christian Olsson <colslund@gmail.com>
2025-11-16 14:56:23 +0100

    css dist update

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/bike-power-tracker/sw.js', { scope: '/bike-power-tracker/' })})}
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    base: './',
+    base: '/bike-power-tracker/',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
